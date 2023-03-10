ae68fef2c1d1025ddc37ec228f250a711e5bac10
Denis <denissand1997@gmail.com>
2023-03-10 13:49:00 +0300

    Task 2: Initial

---
+++ 2 task/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from "./components/Header/header";
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <div className="App">Denis Sambur</div>
+            <div className="Down">Denis Sambur</div>
+        </>
+
+    );
+}
+
+export default App;
---
+++ 2 task/src/components/Header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../../image/logo_welbex.svg"
+
+function Header(props) {
+    return(
+        <>
+            <img src={Logo} alt="Welbex"/>
+        </>
+    )
+}
+
+export default Header
