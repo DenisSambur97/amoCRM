1325896c91f529645d22e9c2dc7913ca9089c1bc
Denis <denissand1997@gmail.com>
2023-03-11 13:46:31 +0300

    Task 2: Adaptive PC + Tablettes

--- 2 task/src/components/Circle/circle.jsx
+++ 2 task/src/components/Circle/circle.jsx
@@ -4,11 +4,6 @@ import style from "./circle.module.sass"
 function Circle() {
     return(
         <div className={style.circle}>
-            {/*<div className="purple-circle"/>*/}
-            {/*<div className="red-circle"/>*/}
-            {/*<div className="red-circle_small"/>*/}
-            {/*<div className="purple-light_circle"/>*/}
-            {/*<div className="red-light_circle"/>*/}
             <div className={style.purple_circle}/>
             <div className={style.red_circle}/>
             <div className={style.red_circle_small}/>
